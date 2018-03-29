b8d229669bdef7f291fb202f5646d270cd0367ca
abhisheksavekar61 <abhisheksavekar61@gmail.com>
2018-03-30 01:52:35 +0530

    add existing file

---
+++ WebstormProjects/Portfolio/Progressbar.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.html').animate({width: '90%'}, 2000);
+    $('.css').animate({width: '90%'}, 2000);
+    $('.javascript').animate({width: '85%'}, 2000);
+    $('.nodejs').animate({width: '75%'}, 2000);
+    $('.vuejs').animate({width: '70%'}, 2000);
+    $('.reactjs').animate({width: '70%'}, 2000);
+});

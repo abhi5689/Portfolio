9866d77f77d476b30a4e25597a59d0a4984d2759
abhisheksavekar61 <abhisheksavekar61@gmail.com>
2018-04-01 23:00:42 +0530

    1/4/2018

--- WebstormProjects/Portfolio/index.js
+++ WebstormProjects/Portfolio/index.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
+$('a.scrollto').on('click', function () {
+   var scrollto = $(this).attr('data-scrollto');
     $('.html').animate({width: '90%'}, 2000);
     $('.css').animate({width: '90%'}, 2000);
     $('.javascript').animate({width: '85%'}, 2000);
     $('.nodejs').animate({width: '75%'}, 2000);
     $('.vuejs').animate({width: '70%'}, 2000);
     $('.reactjs').animate({width: '70%'}, 2000);
-
+    $('body, html').animate({
+       "scrollTop": $('#'+scrollto).offset().top-100
+   },500);
+   return false;
 });
+});
+

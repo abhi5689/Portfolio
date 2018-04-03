a9f40c5b82986cf724c08fa879c05234a47cc228
abhisheksavekar61 <abhisheksavekar61@gmail.com>
2018-04-03 23:41:39 +0530

    3/4/2018

--- WebstormProjects/Portfolio/index.js
+++ WebstormProjects/Portfolio/index.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
-$('a.scrollto').on('click', function () {
-   var scrollto = $(this).attr('data-scrollto');
-    $('.html').animate({width: '90%'}, 2000);
-    $('.css').animate({width: '90%'}, 2000);
-    $('.javascript').animate({width: '85%'}, 2000);
-    $('.nodejs').animate({width: '75%'}, 2000);
-    $('.vuejs').animate({width: '70%'}, 2000);
-    $('.reactjs').animate({width: '70%'}, 2000);
-    $('body, html').animate({
-       "scrollTop": $('#'+scrollto).offset().top-100
-   },500);
-   return false;
-});
+
+    $(window).on('scroll',function () {
+    var skillposition = $('#skills').offset().top-$(window).scrollTop()- $(window).outerHeight()
+        if(skillposition < - 530) {
+            $('.html').animate({width: '90%'}, 2000);
+            $('.css').animate({width: '90%'}, 2000);
+            $('.javascript').animate({width: '85%'}, 2000);
+            $('.nodejs').animate({width: '75%'}, 2000);
+            $('.vuejs').animate({width: '70%'}, 2000);
+            $('.reactjs').animate({width: '70%'}, 2000);
+        }
+    });
+
+    $('a.scrollto').on('click', function () {
+        var scrollto = $(this).attr('data-scrollto');
+
+        $('body, html').animate({
+            "scrollTop": $('#' + scrollto).offset().top - 100
+        }, 500);
+        return false;
+    });
 });
 
